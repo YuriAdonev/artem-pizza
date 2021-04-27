e9bc817ed68e36226ecb06f934a5c086a8453129
Yuri Adonev <zyztem@mail.ru>
2021-04-27 17:20:34 +0700

    refactor: move SUT creation to a factory method

--- src/presentation/pages/PizzaConfigurator/PizzaConfiguratorItem.spec.tsx
+++ src/presentation/pages/PizzaConfigurator/PizzaConfiguratorItem.spec.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import { PizzaConfiguratorItem, PizzaConfiguratorItemProps } from './PizzaConfiguratorItem'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (props: PizzaConfiguratorItemProps): SutTypes => {
+  const sut = render(<PizzaConfiguratorItem {...props} />)
+  return {
+    sut
+  }
+}
 
 describe('PizzaConfiguratorItem', () => {
   test('Should render radio inputs without type in the props', () => {
-    const props: PizzaConfiguratorItemProps = {
+    const props = {
       title: 'any_title',
       name: 'any_name',
       values: ['any_value1', 'any_value2']
     }
-    const sut = render(<PizzaConfiguratorItem {...props} />)
+    const { sut } = makeSut(props)
     const wrapper = sut.getByTestId('pizza-configurator-item-wrapper')
     const inputs = wrapper.querySelectorAll('input[type="radio"]')
     expect(inputs.length).toBe(props.values.length)
