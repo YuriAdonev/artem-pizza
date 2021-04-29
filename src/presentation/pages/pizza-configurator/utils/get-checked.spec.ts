a7c1009479e9186b8a5178df224c85ef5419b006
Yuri Adonev <zyztem@mail.ru>
2021-04-30 03:25:00 +0700

    test: ensure getChecked returns false if value not equal selected on type single

--- src/presentation/pages/pizza-configurator/utils/get-checked.spec.ts
+++ src/presentation/pages/pizza-configurator/utils/get-checked.spec.ts
@@ -12,4 +12,8 @@ describe('Utils - getChecked', () => {
   test('Should return true if value includes in selected on type multiply', () => {
     expect(getChecked({ type: 'multiply', value: 'current_value', selected: ['current_value'] })).toBe(true)
   })
+
+  test('Should return false if value not equal selected on type single', () => {
+    expect(getChecked({ type: 'single', value: 'current_value', selected: 'any_value' })).toBe(false)
+  })
 })
