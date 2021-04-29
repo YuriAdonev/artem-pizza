14e67be5e9d9089ace4696d6ed19beb8cce6bc64
Yuri Adonev <zyztem@mail.ru>
2021-04-30 03:34:22 +0700

    test: ensure getInputType returns checkbox if type multiply

--- src/presentation/pages/pizza-configurator/utils/get-input-type.spec.ts
+++ src/presentation/pages/pizza-configurator/utils/get-input-type.spec.ts
@@ -8,4 +8,8 @@ describe('Utils - getInputType', () => {
   test('Should return radio if type single', () => {
     expect(getInputType('single')).toBe('radio')
   })
+
+  test('Should return checkbox if type multiply', () => {
+    expect(getInputType('multiply')).toBe('checkbox')
+  })
 })
