032ac45b9ccee97b48bd81ac35c6182c3b78dffd
Yuri Adonev <zyztem@mail.ru>
2021-04-30 03:16:20 +0700

    test: ensure getChecked returns false if value not includes in selected on type multiply

--- src/presentation/pages/pizza-configurator/utils/get-checked.spec.ts
+++ src/presentation/pages/pizza-configurator/utils/get-checked.spec.ts
@@ -4,4 +4,8 @@ describe('Utils - getChecked', () => {
   test('Should return false on default', () => {
     expect(getChecked({ type: '', value: '', selected: '' })).toBeFalsy()
   })
+
+  test('Should return false if value not includes in selected on type multiply', () => {
+    expect(getChecked({ type: 'multiply', value: 'current_value', selected: ['any_value'] })).toBeFalsy()
+  })
 })
