660257fa273d7ca82908fd9cd201a3ee24bf20d1
Yuri Adonev <zyztem@mail.ru>
2021-04-29 11:18:45 +0700

    refactor: move SUT creation to a factory method

--- src/presentation/pages/PizzaConfigurator/PizzaConfigurator.spec.tsx
+++ src/presentation/pages/PizzaConfigurator/PizzaConfigurator.spec.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import { pizzaConfiguratorFields } from './config'
 import { PizzaConfigurator } from './PizzaConfigurator'
 
+const makeSut = (): RenderResult => render(<PizzaConfigurator />)
+
 describe('PizzaConfigurator', () => {
   test('Should render all form fields', () => {
-    const sut = render(<PizzaConfigurator />)
+    const sut = makeSut()
     const fieldsWrapper = sut.getByTestId('pizza-configurator-fields')
     pizzaConfiguratorFields.forEach(field => {
       field.values.forEach(value => {
