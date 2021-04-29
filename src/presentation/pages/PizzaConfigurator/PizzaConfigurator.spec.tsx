9d878e6c0726e2123e2cb24833cad82e2ae7ca56
Yuri Adonev <zyztem@mail.ru>
2021-04-29 12:21:33 +0700

    test: ensure PizzaConfigurator renders with initialState on start

--- src/presentation/pages/PizzaConfigurator/PizzaConfigurator.spec.tsx
+++ src/presentation/pages/PizzaConfigurator/PizzaConfigurator.spec.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
-import { pizzaConfiguratorFields } from './config'
+import { render, RenderResult, cleanup } from '@testing-library/react'
+import { pizzaConfiguratorFields, pizzaConfiguratorInitialState } from './config'
 import { PizzaConfigurator } from './PizzaConfigurator'
 
 const makeSut = (): RenderResult => render(<PizzaConfigurator />)
 
 describe('PizzaConfigurator', () => {
+  afterEach(cleanup)
+
   test('Should render all form fields', () => {
     const sut = makeSut()
     const fieldsWrapper = sut.getByTestId('pizza-configurator-fields')
@@ -15,5 +17,24 @@ describe('PizzaConfigurator', () => {
         expect(input).toBeTruthy()
       })
     })
+  })
+
+  test('Should render with initialState on start', () => {
+    const sut = makeSut()
+    const fieldsWrapper = sut.getByTestId('pizza-configurator-fields')
+    const configuration = {}
+    pizzaConfiguratorFields.forEach(field => {
+      const inputs = fieldsWrapper.querySelectorAll<HTMLInputElement>(`input[name="${field.name}"]`)
+      const checkedInputs = []
+      inputs.forEach(input => input.checked && checkedInputs.push(input.value))
+      if (field.type === 'single') {
+        expect(checkedInputs.length).toBe(1)
+        configuration[field.name] = checkedInputs[0]
+      }
+      if (field.type === 'multiply') {
+        configuration[field.name] = checkedInputs
+      }
+    })
+    expect(configuration).toEqual(pizzaConfiguratorInitialState)
   })
 })
