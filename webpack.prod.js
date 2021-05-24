a3f7e75aa920363bd3ce4cd27143b0e275fa57ab
Yuri Adonev <zyztem@mail.ru>
2021-05-24 22:16:04 +0700

    feat: add cdn to external dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -26,7 +26,9 @@ module.exports = merge(common, {
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM'
+    axios: 'axios',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM'
   },
   plugins: [
     new HtmlWebpackPlugin({
